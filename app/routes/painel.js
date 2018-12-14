b4a8e7a1a199df3b800508868b060b4063b66cdc
André Valle <andrevvalle@icloud.com>
2018-12-14 18:44:19 -0200

    Config router painel - Config return promisse posts list - Config post loop view

--- app/routes/painel.js
+++ app/routes/painel.js
@@ -1,8 +1,17 @@
-import PostFeed from '../controllers/postfeed';
+import * as PostfeedModel from '../models/postfeed';
 
 module.exports = api => {
 	api.get('/painel', (req, res) => {
-        res.render('painel');
+        const defer = PostfeedModel.GetAll();
+
+        defer.then(posts => {
+            res.render('painel', { listPosts: posts });
+        });
+
+        defer.catch(err => {
+            res.status(400);
+            res.json({message: err.message});
+        });
     });
 	
     api.get('/painel/newpost', (req, res) => {
