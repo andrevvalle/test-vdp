e8054d1b4ff4fe2503ecce1f436c552cac909b76
André Valle <andrevvalle@icloud.com>
2018-12-15 21:16:58 -0200

    Refactoring routes painel

--- app/routes/painel.js
+++ app/routes/painel.js
@@ -1,20 +1,9 @@
-import * as PostfeedModel from '../models/postfeed';
+import PainelController from '../controllers/painel';
 
 module.exports = api => {
-	api.get('/painel', (req, res) => {
-        const defer = PostfeedModel.GetAll();
+    const painelController = new PainelController();
 
-        defer.then(posts => {
-            res.render('painel', { listPosts: posts });
-        });
-
-        defer.catch(err => {
-            res.status(400);
-            res.json({message: err.message});
-        });
-    });
-	
-    api.get('/painel/newpost', (req, res) => {
-        res.render('painel/newpost');
-    });
+    api.route('/painel').get(painelController.getHomePainel)
+    api.route('/painel/newpost').get(painelController.getNewpostPage)
+    api.route('/painel/editpost/:postId').get(painelController.getEditpostPage)
 };
